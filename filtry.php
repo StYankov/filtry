cdf12989fc34b319ad1878e7831040f28332e5a2
StYankov <st.yankov00@gmail.com>
2024-11-15 08:19:03 +0200

    fix: update plugin license

--- filtry.php
+++ filtry.php
@@ -3,14 +3,14 @@
  * Plugin Name: Filtry
  * Plugin URI: https://filtry.stoilyankov.com/
  * Description: Lightning-fast WooCommerce filters that make shopping a breeze! More finds, more sales, less fuss!
- * Version: 0.83
+ * Version: 0.84
  * Author: Stoil Yankov
  * Author URI: https://stoilyankov.com
  * Text Domain: filtry
  * Domain Path: /languages/
  * Requires at least: 6.2
  * Requires PHP: 8.1
- *
+ * License: GPL2
  */
 
 defined( 'ABSPATH' ) || exit;
